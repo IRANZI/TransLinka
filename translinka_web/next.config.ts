e704f48fcb6fc4cae4ddb7e8f6cb9d8971d478aa
IRANZI <iradianah5@gmail.com>
2025-08-21 10:33:10 +0200

    Disable ESLint blocking for Vercel build

--- translinka_web/next.config.ts
+++ translinka_web/next.config.ts
@@ -1,8 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  eslint: {
+
+    ignoreDuringBuilds: true,
+  },
   experimental: {
-    turbo: false,
+    turbo: {}, 
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
